61105ad8ad31775d662c8d4a44f2ddd4ab350483
nguyenthaituan <12c6nguyenthaituan@gmail.com>
2020-04-25 22:09:35 +0700

    commit2

--- app/src/main/java/vn/edu/ntu/nguyenthaituan_59132942_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/nguyenthaituan_59132942_simplewidget/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
     EditText name, date, favourite;
     Button submit;
     RadioGroup radioGroup;
-    RadioButton radioButton;
-
 
     CheckBox wfilm,lmusic,cf,home,cook;
     @Override
@@ -26,61 +25,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText) findViewById(R.id.tencuaban);
-        date = (EditText) findViewById(R.id.ngaysinhcuaban);
-        favourite = (EditText) findViewById(R.id.sothichkhac);
+        name = findViewById(R.id.tencuaban);
+        date = findViewById(R.id.ngaysinhcuaban);
+        favourite = findViewById(R.id.sothichkhac);
 
         //-------------checkbox---------------
-        wfilm = (CheckBox) findViewById(R.id.xemphim);
-        lmusic = (CheckBox) findViewById(R.id.nghenhac);
-        cf = (CheckBox) findViewById(R.id.dicafe);
-        home = (CheckBox) findViewById(R.id.homealone);
-        cook = (CheckBox) findViewById(R.id.nauan);
+        wfilm = findViewById(R.id.xemphim);
+        lmusic = findViewById(R.id.nghenhac);
+        cf = findViewById(R.id.dicafe);
+        home = findViewById(R.id.homealone);
+        cook =  findViewById(R.id.nauan);
+        radioGroup = findViewById(R.id.radioGroup); //cái này nè
         //------------------------------------
 
-        submit = (Button) findViewById(R.id.buttonxacnhan);
+        submit =  findViewById(R.id.buttonxacnhan);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
         //------------Checkbox-------------------------
+
                 String str ="";
 
+                str+="Tên bạn: "+name.getText().toString();
+                str+="\nNgày sinh: "+date.getText().toString();
+
+                switch (radioGroup.getCheckedRadioButtonId()){
+                    case R.id.nam: str+="\nGiới tính : nam"; break;
+                    case R.id.nu: str+="\nGiới tính : nữ"; break;
+                }
+
+                str+="\nSỏ thích: ";
+
                 if(wfilm.isChecked()){
-                    str =str+wfilm.getText();
+                    str =str+wfilm.getText().toString()+"; ";
                 }
 
                 if(lmusic.isChecked()){
-                    str =str+lmusic.getText();
+                    str =str+lmusic.getText().toString()+"; ";
                 }
 
                 if(cf.isChecked()){
-                    str =str+cf.getText();
+                    str =str+cf.getText().toString()+"; ";
                 }
                 if(home.isChecked()){
-                    str =str+home.getText();
+                    str =str+home.getText().toString()+"; ";
                 }
                 if(cook.isChecked()){
-                    str =str+cook.getText();
+                    str =str+cook.getText().toString();
                 }
-        //-------------------------------------------------
 
-         //--------------Set radioGroup--------------------
-            radioGroup = findViewById(R.id.radioGroup);
-            int id = radioGroup.getCheckedRadioButtonId();
-            radioButton=findViewById(id);
-        //-------------------------------------------------
-                Toast.makeText(getApplicationContext(),"Tên: "+name.getText()+ '\n'
-                        +"Ngày sinh: "+date.getText() + '\n'
-                        + "Giới tính: "+radioButton.getText()+ '\n'
-                        +"Sở thích: "+" , " +str+", "
-                        +favourite.getText()
-                        , Toast.LENGTH_SHORT).show();
+                str+=favourite.getText()+".";
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
             }
         });
     }
-
-
-
 
 }
